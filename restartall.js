b8f304d21fe6d3c2a4e9ff55d78768e5834ce188
love magic <magic.loveptit@gmail.com>
2021-03-20 14:00:11 +0700

    no restart

--- restartall.js
+++ restartall.js
@@ -3,14 +3,14 @@ const exec = require('child_process').exec;
 
 restartAll = async () => {
 
- console.log("------- Restart all -------")
-    exec("", (error) => {
-    //exec("pm2 restart all", (error) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-    });
+//  console.log("------- Restart all -------")
+//     exec("", (error) => {
+//     //exec("pm2 restart all", (error) => {
+//         if (error) {
+//             console.log(`error: ${error.message}`);
+//             return;
+//         }
+//     });
 }
 
 cron.schedule('*/30 * * * *', async () => {
