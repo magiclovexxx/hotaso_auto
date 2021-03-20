79228d3336cd0e16ed8dd9902675dacd786941de
love magic <magic.loveptit@gmail.com>
2021-03-20 14:06:16 +0700

    auto restart pc

--- restartall.js
+++ restartall.js
@@ -3,16 +3,16 @@ const exec = require('child_process').exec;
 
 restartAll = async () => {
 
-//  console.log("------- Restart all -------")
-//     exec("", (error) => {
-//     //exec("pm2 restart all", (error) => {
-//         if (error) {
-//             console.log(`error: ${error.message}`);
-//             return;
-//         }
-//     });
+ console.log("------- Restart all -------")
+    exec("shutdown -r", (error) => {
+    //exec("pm2 restart all", (error) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+    });
 }
 
-cron.schedule('*/30 * * * *', async () => {
+cron.schedule('*/120 * * * *', async () => {
     await restartAll()
   })
