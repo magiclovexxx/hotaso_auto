1db0e6621a34b31c6979bca20a2bed8b82470fc3
unknown <magic.loveptit@gmail.com>
2021-03-22 04:12:55 +0700

    max page = 20

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ let page
 headersearch = {
 
 cookie: '_gcl_au=1.1.699808476.1607702732; SPC_IA=-1; SPC_EC=-; SPC_F=FXGW8llunAQuf5baIJM19NtcxbG2f9tj; REC_T_ID=b28c0ef6-3bca-11eb-a793-b49691844b48; SPC_U=-; _fbp=fb.1.1607702732348.1633153129; _hjid=0ecfc287-f2da-4004-826d-8ad89e4b90b8; _gcl_aw=GCL.1608656038.EAIaIQobChMIi8qo2Ybi7QIVTT5gCh2--ggXEAYYAiABEgK-1_D_BwE; _med=cpc; _gac_UA-61914164-6=1.1608656040.EAIaIQobChMIi8qo2Ybi7QIVTT5gCh2--ggXEAYYAiABEgK-1_D_BwE; SPC_SI=mall.pFUIKPdyxP5VnhVg50pEFlynHNuJRRuW; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; _gid=GA1.2.1577912680.1609217942; csrftoken=7VQS9mkU5q5Q7WVXoz3ZpaISzMY6pmF0; cto_bundle=oonKA185cGlHMUdYYkQxRyUyQmdadTdzRjJFVk1KRHIycVBUUk1TcHloa3U2eVMwUkkyUnM5bkdvOGJwUUdERVRRZFZSRHQ0VmJaeFhBek9RbVVIMkwyY0FNTU13QXVCWTVGWXExcE1URFRTT25LMXY2UjBqeHpKT00wdXJCZG9hdlZoWjNhUFduTDZMWXJuendJRm5ocnF4TXVDQSUzRCUzRA; _ga_M32T05RVZT=GS1.1.1609303653.16.1.1609307858.0; _ga=GA1.1.802594605.1607702734; SPC_R_T_ID="MHYDKro2Fd4NUQJZR4w7Fo6d9p0Riyckd4IyA9QwUfZ9dHG982W1hn7Bh6ixp6C5652W0aR87Qs0OcPQ1JpOLzC7LCayCB0AgMfqsvAw21s="; SPC_T_IV="JlfVIc0gll7Lnf2hf9gUZw=="; SPC_R_T_IV="JlfVIc0gll7Lnf2hf9gUZw=="; SPC_T_ID="MHYDKro2Fd4NUQJZR4w7Fo6d9p0Riyckd4IyA9QwUfZ9dHG982W1hn7Bh6ixp6C5652W0aR87Qs0OcPQ1JpOLzC7LCayCB0AgMfqsvAw21s="',
-referer: 'https://shopee.vn/search?keyword=v%C3%AD%20n%E1%BB%AF%20%C4%91%E1%BA%B9p',
+referer: 'https://shopee.vn/',
 'if-none-match-': ' 55b03-362c8065febe2677f1d3f36f302b86c8'
 
 }
@@ -37,19 +37,20 @@ runAllTime = async () => {
     //     console.log(error)
 
     // }
-    for(let i=1; i<2; i++){
+    for(let i=0; i<2; i++){
 
         page = 50*i
-        shopeesearch = "https://shopee.vn/api/v2/search_items/?by=relevancy&keyword=v%C3%AD%20n%E1%BB%AF%20%C4%91%E1%BA%B9p&limit=50&newest="+page+"&order=desc&page_type=search&version=2"
+        shopeesearch = "https://shopee.vn/api/v4/search/search_items?by=relevancy&keyword=v%C3%AD%20n%E1%BB%AF&limit=50&newest=50&order=desc&page_type=search&version=2"
         shopInfo = "https://shopee.vn/api/v2/shop/get?shopid=74300615"
         productInfo = "https://shopee.vn/api/v2/item/get?itemid=6705447143&shopid=74300615"
         shopProduct = "https://shopee.vn/api/v2/search_items/?by=pop&entry_point=ShopByPDP&limit=100&match_id=19608398&newest=000&order=desc&page_type=shop"
         keywordApi = "https://shopee.vn/api/v4/search/search_hint?keyword=v%C3%AD%20n%E1%BB%AF&search_type=0&version=1"
-        search_api = "https://shopee.vn/api/v2/search_items/?by=relevancy&keyword=v%C3%AD%20n%E1%BB%AF%20%C4%91%E1%BA%B9p&limit=50&newest=50&order=desc&page_type=search&version=2"
+        search_api = "https://shopee.vn/api/v2/search_items/?by=relevancy&keyword=v%C3%AD%20n%E1%BB%AF&limit=50&newest="+page+"&order=desc&page_type=search&version=2"
+        console.log(shopeesearch)
         try {
-            datatest = await axios.get(search_api, {
+            datatest = await axios.get(shopeesearch, {
               
-               // headers : headersearch
+                headers : headersearch
             })
             
         } catch (error) {
@@ -57,8 +58,8 @@ runAllTime = async () => {
             console.log(error)
         }
         data = datatest.data
-        console.log(datatest.data.items.length)
-        console.log(datatest.data.items[0].name)
+        //console.log(i + ":" + datatest.data.items.name)
+        console.log(datatest.data.items[0].item_basic.name)
         if(data.items != undefined){
             //console.log(i + " --- " +datatest.data.items.length)
             //console.log(datatest.data.items[0].itemid)
