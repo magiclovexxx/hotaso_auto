3c97e543090d6b08626a9b4c91482d3f48f4ec44
love magic <magic.loveptit@gmail.com>
2021-04-18 11:36:16 +0700

    fix khoi dong lai

--- startup.cmd
+++ startup.cmd
@@ -1,3 +1,4 @@
+Shutdown -r -t 3600
 cd C:\Users\Admin\Desktop\hotaso_shopee\
 update.sh
 pm2 log
--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@ git pull origin master
 npm install
 npm install pm2-windows-startup -g
 pm2-startup install
-pm2 start restartall.js
 pm2 start shopee.js
 pm2 save
 pm2 restart all
