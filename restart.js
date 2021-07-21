589d8231bac71f95643d52a8da5f731bfe6f709f
love magic <magic.loveptit@gmail.com>
2021-07-22 02:03:14 +0700

    fix restart all

--- restart.js
+++ restart.js
@@ -5,38 +5,39 @@ require('dotenv').config();
 slavenumber = process.env.SLAVE
 
 restartAll = async () => {
-try {
-    console.log("------- Restart all -------")
+    try {
+        console.log("------- Restart all -------")
 
- let check = await actionsShopee.check_slave_die(slavenumber)
- console.log(check)
- 
- if(check==1){
- //exec("shutdown -r", (error) => {
-    exec("pm2 restart all", (error) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
+        let check = await actionsShopee.check_slave_die(slavenumber)
+        console.log(check)
+
+        if (check == 1) {
+            //exec("shutdown -r", (error) => {
+            exec("pm2 restart all", (error) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+            });
         }
-    });
- }
 
- if(check==2){
-    //exec("shutdown -r", (error) => {
-       exec("shutrown -r", (error) => {
-           if (error) {
-               console.log(`error: ${error.message}`);
-               return;
-           }
-       });
+        if (check == 2) {
+            //exec("shutdown -r", (error) => {
+            exec("shutrown -r", (error) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+            });
+        }
+    } catch (error) {
+        console.log(error)
     }
-} catch (error) {
-    console.log(error)
-}
- 
-   
+
+
 }
 
-async () => {
+(async () => {
     await restartAll()
-  }
+
+})();
