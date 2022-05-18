3e2a92bafc444f24b8e07b92aee23ba51e1bf7fa
love magic <magiclove.loveptit@gmail.com>
2022-05-18 16:22:29 +0700

    check die

--- shopee.js
+++ shopee.js
@@ -140,7 +140,7 @@ loginShopee = async (page, accounts) => {
                 return 2
             }
 
-            let checkblock = await page.$$('svg>path[clip-rule="evenodd"]')
+            let checkblock = await page.$$('form>div>div>div>div>svg>path[clip-rule="evenodd"]')
 
             if (checkblock) {
                 console.log("account bị khoá")
