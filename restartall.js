352e094ccb6ffc4d3d7bd35b0c9b70b6f8251999
love magic <magiclove.loveptit@gmail.com>
2022-12-25 15:53:09 +0700

    fix

--- restartall.js
+++ restartall.js
@@ -7,7 +7,7 @@ mode = process.env.MODE
 
 restartAll = async () => {
 
- console.log("------- Restart all -------")
+    console.log("------- Restart all -------")
     try {
         exec("node restart.js", (error) => {
             if (error) {
@@ -18,23 +18,25 @@ restartAll = async () => {
 
         let check = await actionsShopee.check_slave_die(slavenumber)
         console.log("Trạng thái slave: " + check)
-        if(check){
-        //exec("shutdown -r", (error) => {
-           exec("pm2 restart all", (error) => {
-               if (error) {
-                   console.log(`error: ${error.message}`);
-                   return;
-               }
-           });
+        if (check) {
+            //exec("shutdown -r", (error) => {
+            exec("pm2 restart all", (error) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+            });
         }
     } catch (error) {
         console.log(error)
     }
 }
-if(mode == "DEV"){
-    await restartAll()
-}else{
+if (mode == "DEV") {
+    async () => {
+        await restartAll()
+    }
+} else {
     cron.schedule('*/20 * * * *', async () => {
         await restartAll()
-      })
+    })
 }
